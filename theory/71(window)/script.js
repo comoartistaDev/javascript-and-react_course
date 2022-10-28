b1bf4ac7cf1a74278c789c16cfa2887e4476f4cf
Oksana Melnyk <comoartista.dev@gmail.com>
2022-10-28 10:17:10 +0200

    Update script.js

--- theory/71(window)/script.js
+++ theory/71(window)/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+const box = document.querySelector('.box'),
+      btn = document.querySelector('button');
+
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
+
+// console.log(width, height);
+
+//show all element, after click on button
+// btn.addEventListener('click', () => { 
+//   box.style.height = box.scrollHeight + 'px';
+// })
+
+// btn.addEventListener('click', () => { 
+//   console.log(box.scrollTop); //show how many px we scrolled, after click
+// })
+
+
+// console.log(box.getBoundingClientRect()); //get coordinates
+// console.log(box.getBoundingClientRect().top); //get only top
+
+// const style = window.getComputedStyle(box); //styles that are applied to the element
+// console.log(style.overflow);
+
+// console.log(document.documentElement.clientWidth);
