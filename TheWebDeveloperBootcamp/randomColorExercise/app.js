80e9257ef07dca75a02451d19d59eb0d4bfbcc7b
Oksana Melnyk <comoartista.dev@gmail.com>
2022-12-24 23:59:01 +0100

    update randomColorExercise

--- TheWebDeveloperBootcamp/randomColorExercise/app.js
+++ TheWebDeveloperBootcamp/randomColorExercise/app.js
@@ -2,12 +2,15 @@ const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
 const body = document.querySelector('body');
 
-button.addEventListener('click', () => {
-  console.log('ckick');
-  const colorRed = Math.floor(Math.random() * 255);
-  const colorGreen = Math.floor(Math.random() * 255);
-  const colorBlue = Math.floor(Math.random() * 255);
-  const newColor = `rgb(${colorRed},${colorGreen},${colorBlue})`;
+button.addEventListener('click', () => {  
+  const newColor = makeRandomColor();
   body.style.backgroundColor = newColor;
   h1.innerText = newColor;
 })
+
+const makeRandomColor = () => {
+  const colorRed = Math.floor(Math.random() * 255);
+  const colorGreen = Math.floor(Math.random() * 255);
+  const colorBlue = Math.floor(Math.random() * 255);
+  return `rgb(${colorRed},${colorGreen},${colorBlue})`;
+}
